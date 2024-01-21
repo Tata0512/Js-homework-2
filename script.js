7f97ae17d9f28b50e1e2fe065fead902c24e9d95
Tata <tata0512@mail.ru>
2024-01-22 01:05:21 +0500

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt("Введите имя...");
+console.log(name)
+var age = prompt("Ваш возраст");
+console.log(age);
+result = prompt("Давай решим несколько примеров? 8-)");
+
+var example1  = prompt('Сколько будет? 6 + 3 =');
+console.log(example1);
+var example2  = prompt('Сколько будет? 35 - 25 =');
+console.log(example2);
+var example3  = prompt('A если умножим? 5 * 3 =');
+console.log(example3);
+var example4  = prompt('Тогда и поделим? 25 / 5 = ');
+console.log(example4);
+
+result = prompt("Отлично!!! Остался последний! 8-)");
+var X = 5
+var Y = 3
+var Z = 8
+var res = (X + Y + Z) / 2;
+var answer = prompt ("Давай вычислим среднее арифметическое XYZ,  если X=5, Y= 3, а Z=8")
+alert('среднее арифметическое XYZ: ' + res)
